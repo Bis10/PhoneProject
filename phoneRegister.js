b022df312e307773547ed3948b69c593379970f2
Suyel Bishnu <suyel2007@gmail.com>
2024-09-09 11:13:23 +0300

    Add new implementation

--- phoneRegister.js
+++ phoneRegister.js
@@ -1,24 +1,57 @@
-'use strict';
+"use strict";
 
-module.exports = class PhoneRegister{
-    #register
-    constructor(data){
-        if(!data){
-            throw new Error('phone data missing');
-        }
-        this.#register = data;
+module.exports = class PhoneRegister {
+  #register;
+  constructor(data) {
+    if (!data) {
+      throw new Error("phone data missing");
     }
+    this.#register = data;
+  }
 
-    getTypes(){
-        const foundTypes = [];
-        for (const person of this.#register){
-            for(const phone of person.phones){
-                if(!foundTypes.includes(phone.type)){
-                    foundTypes.push(phone.type);
-            }
+  getTypes() {
+    const foundTypes = [];
+    for (const person of this.#register) {
+      for (const phone of person.phones) {
+        if (!foundTypes.includes(phone.type)) {
+          foundTypes.push(phone.type);
         }
+      }
     }
-        return foundTypes;
+    return foundTypes;
+  }
+  //   getPersonsNumberByType(firstname, lastname, type) {
+  //     if (arguments.length < 3) {
+  //       throw new Error("missing parameter");
+  //     }
+  //     const numbersFound = [];
+  //     for (const person of this.#register) {
+  //       if (person.firstname === firstname && person.lastname === lastname) {
+  //         for (const phone of person.phones) {
+  //           if (phone.type === type) {
+  //             numbersFound.push(phone.number);
+  //           }
+  //         }
+  //       }
+  //     }
+  //     return numbersFound;
+  //   }
+
+  getPersonsNumberByType(firstname, lastname, type) {
+    if (arguments.length < 3) {
+      throw new Error("missing parameter");
     }
-    
-}
+    const numbersFound = [];
+    for (const person of this.#register) {
+      if (person.firstname === firstname && person.lastname === lastname) {
+        for (const phone of person.phones) {
+          if (phone.type === type) {
+            numbersFound.push(phone.number);
+          }
+        }
+           return numbersFound;
+      }
+    }
+    return numbersFound;
+  }
+}; //End of the class
