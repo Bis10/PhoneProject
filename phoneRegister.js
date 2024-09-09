2a2a17b04ecf3d517d6f8b748e1cfd8ba55ccceb
Suyel Bishnu <suyel2007@gmail.com>
2024-09-09 10:19:16 +0300

    Create implementation for getTypes

--- phoneRegister.js
+++ phoneRegister.js
@@ -1,7 +1,24 @@
 'use strict';
 
 module.exports = class PhoneRegister{
+    #register
+    constructor(data){
+        if(!data){
+            throw new Error('phone data missing');
+        }
+        this.#register = data;
+    }
 
-
+    getTypes(){
+        const foundTypes = [];
+        for (const person of this.#register){
+            for(const phone of person.phones){
+                if(!foundTypes.includes(phone.type)){
+                    foundTypes.push(phone.type);
+            }
+        }
+    }
+        return foundTypes;
+    }
     
 }
